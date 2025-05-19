eeec166407702178f9b2c4c334e7604536c3a445
sarou1255 <sarou1255@gmail.com>
2025-05-19 14:21:34 +0100

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Route, Routes } from "react-router-dom";
 import Nav from "./common/navbar";
 import Home from "./home/home";
 import About from "./about/about";
+import HOmeProducts from "./home/home-products";
+import Footer from "./common/footer";
 
 const App = () => (
   <div>
@@ -10,6 +12,8 @@ const App = () => (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/home#products" element={<HOmeProducts />} />
+      <Route path="/home#about" element={<Footer />} />
       <Route path="/about" element={<About />} />
     </Routes>
   </div>
