cd628cbc6b7bde75f6e88b0bb0ff2e36a2606322
sarou1255 <sarou1255@gmail.com>
2025-05-21 20:19:37 +0100

    s

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
-import vercel from 'vite-plugin-vercel';
- 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
   server: {
     port: 3045,
   },
-  plugins: [vercel()],
-});
+  plugins: [react()],
+})
